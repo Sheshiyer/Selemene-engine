e87cecdcd07eb6fd5d679b3688ed3061d1cda5a2
sheshiyer <sheshnarayan.iyer@gmail.com>
2026-06-14 19:05:38 +0530

    chore: remove generated build artifacts

--- standalone_demo/target/debug/deps/standalone_demo-e7edc968f74efb49.d
+++
@@ -1,5 +0,0 @@
-/Users/sheshnarayaniyer/2025/witnessos/Selemene-engine/standalone_demo/target/debug/deps/standalone_demo-e7edc968f74efb49.d: src/main.rs
-
-/Users/sheshnarayaniyer/2025/witnessos/Selemene-engine/standalone_demo/target/debug/deps/standalone_demo-e7edc968f74efb49: src/main.rs
-
-src/main.rs:
